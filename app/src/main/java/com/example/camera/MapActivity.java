60a3d0461bda44a0379c09367027b3cac286dc37
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-07-05 16:28:28 +0800

    实现经纬度转换功能并在百度地图上定位图片位置

--- app/src/main/java/com/example/camera/MapActivity.java
+++ app/src/main/java/com/example/camera/MapActivity.java
@@ -1,6 +1,7 @@
 package com.example.camera;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,22 +13,61 @@ import androidx.core.app.ActivityCompat;
 
 import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends Activity {
     private MapView mMapView = null;
+    @RequiresApi(api = Build.VERSION_CODES.M-1)
+    void Request() {             //获取相机拍摄读写权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//版本判断
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA,Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.INTERNET,Manifest.permission.READ_PHONE_STATE,Manifest.permission.READ_SYNC_SETTINGS,
+                        Manifest.permission.ACCESS_WIFI_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WAKE_LOCK}, 1);
+            }
+        }
+    }
+    private double change(String string) {
+        double num = 0.0;
+        if (null==string){
+            return num;
+        }
+        //用 ，将数值分成3份
+        String[] split = string.split(",");
+        for (int i = 0; i < split.length; i++) {
 
+            String[] s = split[i].split("/");
+            //用112/1得到度分秒数值
+            double v = Double.parseDouble(s[0]) / Double.parseDouble(s[1]);
+            //将分秒分别除以60和3600得到度，并将度分秒相加
+            num=num+v/Math.pow(60,i);
+        }
+        return num;
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String latitude = getIntent().getStringExtra("LATITUDE");
+        String longitude = getIntent().getStringExtra("LONGITUDE");
+        double lat = change(latitude);
+        double lon = change(longitude);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
-        SDKInitializer.initialize(this);
-        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
-        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
-        SDKInitializer.setCoordType(CoordType.BD09LL);
         mMapView = findViewById(R.id.bmapView);
+        BaiduMap baiduMap = mMapView.getMap();
+        MapStatusUpdate update = MapStatusUpdateFactory.zoomTo(12.5f);
+        baiduMap.animateMapStatus(update);
+        LatLng ll = new LatLng(lat,lon);
+        MapStatusUpdate update1 = MapStatusUpdateFactory.newLatLng(ll);
+        baiduMap.animateMapStatus(update1);
+        Request();
     }
-
     @Override
     protected void onResume() {
         super.onResume();
